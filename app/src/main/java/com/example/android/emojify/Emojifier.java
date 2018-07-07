2cf848c53640e470f36c62e14bc6cb2a67e2c866
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2018-07-07 16:31:17 -0400

    Get probabilities the face in the image is smiling and whether each eye is open or not.

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -56,13 +56,27 @@ class Emojifier {
         // If there are no faces detected, show a Toast message
         if(faces.size() == 0){
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
-        }
+        } else {
+            // DONE (2): Iterate through the faces, calling getClassifications() for each face.
+            for (int i = 0; i < faces.size(); ++i) {
+                Face face = faces.valueAt(i);
 
-        // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+                // Log the classification probabilities for each face.
+                getClassifications(face);
+            }
+        }
 
         // Release the detector
         detector.release();
     }
 
-    // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    // DONE (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    private static void getClassifications(Face face){
+        // Log all the probabilities
+        Log.d(LOG_TAG, "smiling probability = " + face.getIsSmilingProbability());
+        Log.d(LOG_TAG, "probability left eye is open = "
+                + face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "probability right eye is open = "
+                + face.getIsRightEyeOpenProbability());
+    }
 }
